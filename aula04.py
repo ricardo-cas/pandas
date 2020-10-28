1e96bfc54feb64cc7d22836149e8089c624f3103
Ricardo Costa <ricardo_cas4@hotmail.com>
2020-10-28 18:44:58 -0300

    aula finalizada

--- aula04.py
+++ aula04.py
@@ -1,19 +1,25 @@
 import pandas as pd
 
+# importando dados 
+dados = pd.read_csv('db.csv', sep = ';', index_col=0)
 
+# Iterando DataFrames
+def carrega_dados():
+    dados = pd.read_csv('db.csv', sep = ';', index_col=0)
+    print(dados.head())
 
-def hello():
-    print(".______        ___      .__   __.  _______       ___           _______.")
-    print("|   _  \      /   \     |  \ |  | |       \     /   \         /       |")
-    print("|  |_)  |    /  ^  \    |   \|  | |  .--.  |   /  ^  \       |   (----`")
-    print("|   ___/    /  /_\  \   |  . `  | |  |  |  |  /  /_\  \       \   \    ")
-    print("|  |       /  _____  \  |  |\   | |  '--'  | /  _____  \  .----)   |   ")
-    print("| _|      /__/     \__\ |__| \__| |_______/ /__/     \__\ |_______/    ")
-
-
+def iterando_dataset():
+    # dados = pd.read_csv('db.csv', sep = ';', index_col=0)
+    for index, linha in dados.iterrows():
+        if (2020 - linha['Ano'] != 0):
+            dados.loc[index, 'Km_media'] = linha['Quilometragem'] / 2020 - linha['Ano']
+        else:
+            dados.loc[index, 'Km_media'] = 0
+print(dados.describe())
 
 def __main():
-    hello()
+    carrega_dados()
+    iterando_dataset()
 
 
 if __name__ == "__main__":
