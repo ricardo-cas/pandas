f36aa8dcd2eaf741daf71f7b375be2b44d0d954c
Ricardo Costa <ricardo_cas4@hotmail.com>
2020-10-28 19:03:27 -0300

    criado metodo para substituir informações nulas no data frame

--- aula05.py
+++ aula05.py
@@ -4,12 +4,33 @@ import pandas as pd
 
 dados = pd.read_csv('db.csv', sep = ';', index_col=0)
 
-def __main():
+def carrega():
     print('Pandas Starting....')
     print(dados.head())
     print('\n')
     # Mostra algumas informações do Dataset de forma rápida
     print(dados.info())
+
+
+def __main():
+    carrega()
+    selecionando_informacoes_nulas()
+    substituindo_informacoes_nulas
+
+
+def selecionando_informacoes_nulas():
+    print('selecionando os valores nulos da coluna "Quilometragem" \n')
+    dados.Quilometragem.isna()
+    print('\n')
+    print('Retorno dos valores nulos da coluna "Quilometragem" em forma de Series \n')
+    print(dados.Quilometragem.isna())
+    print('\n')
+    print('Com essas Series podemos passar ela para o DataFrame em forma de Paramêtro, para que ele faça um Select \n')
+    print(dados[dados.Quilometragem.isna()])
+    print('\n')
+
+def substituindo_informacoes_nulas():
+    print('Hello')
 
 if __name__ == "__main__":
     __main()
