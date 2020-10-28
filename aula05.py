8d1d3aefb86e2e9c8a1279e92d7022ff879c84ad
Ricardo Costa <ricardo_cas4@hotmail.com>
2020-10-28 18:51:45 -0300

    utilizando o metodo info, para ver informações mais rapidas

--- aula05.py
+++ aula05.py
@@ -1,9 +1,15 @@
 import pandas as pd
 
+# Tratamento de dados
+
 dados = pd.read_csv('db.csv', sep = ';', index_col=0)
 
 def __main():
     print('Pandas Starting....')
+    print(dados.head())
+    print('\n')
+    # Mostra algumas informações do Dataset de forma rápida
+    print(dados.info())
 
 if __name__ == "__main__":
     __main()
