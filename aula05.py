92e2605f9448eada6d27e42e58860acec8f7c35b
Ricardo Costa <ricardo_cas4@hotmail.com>
2020-10-28 19:16:11 -0300

    Arquivo finalizado

--- aula05.py
+++ aula05.py
@@ -15,7 +15,8 @@ def carrega():
 def __main():
     carrega()
     selecionando_informacoes_nulas()
-    substituindo_informacoes_nulas
+    substituindo_informacoes_nulas()
+    eliminando_valores_nulos_no_dataframe()
 
 
 def selecionando_informacoes_nulas():
@@ -30,7 +31,27 @@ def selecionando_informacoes_nulas():
     print('\n')
 
 def substituindo_informacoes_nulas():
-    print('Hello')
+    print('Aqui estou modificando somente visualmente os dados nulos por 0, não está gravando efetivamente no DataFrame')
+    dados.fillna(0)
+    print(dados.fillna(0))
+    print('\n')
+    print('Aqui estou modificando os dados nulos por 0, e gravando efetivamente no DataFrame')
+    dados.fillna(0, inplace = True)
+    print('\n')
+    print(dados)
+
+
+def eliminando_valores_nulos_no_dataframe():
+    import pandas as pd
+    carros = pd.read_csv('db.csv', sep= ';')
+    print('### Novo Data Frame ### \n')
+    print(carros.head())
+    print('Eliminando os dados nulos do DataFrame utiliando o metodo Dropna: \n')
+    # utilizando o dropna você precisa falar qual coluna que você quer que ele procure os dados à serem deletados do DataFrame
+    # E para que essa remoção seja efetiva, é necessário passar o paramento inplace = True
+    carros.dropna(subset = ['Quilometragem'], inplace = True)
+    print('Resultado da eliminação: \n')
+    print(carros)
 
 if __name__ == "__main__":
     __main()
