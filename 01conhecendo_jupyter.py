43e6c42789f058a03a0d790394874b9b959ac6dd
Ricardo Costa <ricardo_cas4@hotmail.com>
2020-10-28 23:43:18 -0300

    Arquivo atualizado com a primeira aula do curso

--- 01conhecendo_jupyter.py
+++ 01conhecendo_jupyter.py
@@ -1,1 +1,6 @@
+def intro():
+    print('Nesta aula foi somente apresentado a ferramenta de edição de dados, o Jupyter Notebook e o ambiente do Anaconda')
 
+
+if __name__ == "__main__":
+    intro()
