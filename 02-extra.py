dad73ac826e334b173e8bc2bb8d4ab9668f37779
Ricardo Costa <ricardo_cas4@hotmail.com>
2020-11-01 18:36:08 -0300

    abrindo arquivo .json com pandas

--- 02-extra.py
+++ 02-extra.py
@@ -1,8 +1,14 @@
 import pandas as pd
 
 def carrega():
-    dados = pd.read_csv('aluguel.csv', sep= ';')
-    print(dados.head(10))
+    json = open('extras/dados/aluguel.json')
+    # mostrando as informações sobre a variavel
+    print(json) 
+    print('\n')
+    # para realizar a leitura do arquivo JSON, é necessário utilizar o método .read()
+    # por default, ele irá mostrar as informações dos 10 primeiros registros do arquivo
+    print(json.read())
+    
 
 
 
